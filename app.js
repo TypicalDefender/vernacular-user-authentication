22cb7470447c069979be28792e53ca40543bb2b1
i_sarthak <sarthaka.dalabehera@vedantu.com>
2020-04-23 19:18:04 +0530

    server setup

---
+++ app.js
@@ -0,0 +1,33 @@
+//import modules
+const config = require('config');
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const app = express();
+
+//immediately invoked function expression
+//database connection function
+(async function connectDB() {
+    try {
+        const URI = config.get(app.get('env')).MONGO_URI;
+        const connect = await mongoose.connect(URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+        console.log("Connected to the database:", connect.connections[0].name);
+    } catch (err) {
+        console.log("err connecting the database", err);
+    }
+})();
+
+//morgan for development environment
+if (app.get('env') == "development") {
+    app.use(morgan('tiny'));
+}
+
+//port setup
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`magic happens on ${PORT}`);
+});
